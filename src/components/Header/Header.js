a9de051d4cce37de65196a401793b0d209a2d3a0
ahanafislam <ahanafislam@gmail.com>
2022-04-12 05:39:05 +0600

    Create Shipment

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,16 @@ import React from 'react';
 import "./Header.css"
 import logo from "../../images/Logo.svg"
 import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <nav className="header">
            <img src={logo} alt="Site's Logo" />
@@ -12,7 +20,12 @@ const Header = () => {
                 <Link to="/orders">Orders</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/about">About</Link>
-                <Link to="/login">Login</Link>
+                {   
+                    user ?
+                    <button onClick={handleSignOut}>Sign Out</button>
+                    :
+                    <Link to="/login">Login</Link>
+                }
             </div>
         </nav>
     );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from "../../firebase.init";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    const handleConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value);
+    }
+
+    // Redirect to shop page after signup
+    
+    if(user){
+        navigate("/shop");
+    }
+
+    const handleCreateUser = event => {
+        event.preventDefault();
+        if(password !== confirmPassword) {
+            setError('Your two passwords did not match');
+            return;
+        }
+
+        if(password.length < 6) {
+            setError('Password must be 6 characters or longer');
+        }
+
+        createUserWithEmailAndPassword(email, password);
+    }
+
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>Sign Up</h2>
-                <form>
+                <form onSubmit={handleCreateUser}>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" required/>
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id=""  required/>
+                        <input onBlur={handlePasswordBlur} type="password" name="password" id=""  required/>
                     </div>
                     <div className="input-group">
                         <label htmlFor="confirm-password">Confirm Password</label>
-                        <input type="password" name="confirm-password" id="" />
+                        <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" />
                     </div>
-                    <p style={{color: 'red'}}></p>
-                    <input className='form-submit' type="submit" value="Sign Up"  required/>
+                    <p style={{color: 'red'}}>{error}</p>
+                    <input className='form-submit' type="submit" value="Sign Up"/>
                 </form>
                 <p>
                     Already Have an account? <Link className='form-link' to="/login">Login</Link>
